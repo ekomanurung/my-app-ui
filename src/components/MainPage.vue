401915a2466020f3c11c43f977774257d329645b
eko.j.manurung <eko.j.manurung@gdn-commerce.com>
2020-07-17 19:08:10 +0700

    add section popular locations

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,8 +1,43 @@
 <template>
   <div class="container">
     <section id="main-banner"></section>
+    <section id="popular-location" class="popular-location">
+      <h4 class="section-title blue-text text-lighten-1 center">Popular Destinations</h4>
+      <div class="row">
+        <div class="col s12 m4">
+          <img class="responsive-img popular-image" src="../assets/images/bali.jpg" alt="bali" />
+        </div>
+        <div class="col s12 m4">
+          <img class="responsive-img popular-image" src="../assets/images/toba.jpg" alt="toba" />
+        </div>
+        <div class="col s12 m4">
+          <img class="responsive-img popular-image" src="../assets/images/lombok.jpg" alt="lombok" />
+        </div>
+        <div class="col s12 m4">
+          <img
+            class="responsive-img popular-image"
+            src="../assets/images/raja-ampat.jpeg"
+            alt="raja ampat"
+          />
+        </div>
+        <div class="col s12 m4">
+          <img
+            class="responsive-img popular-image"
+            src="../assets/images/bandung.jpg"
+            alt="bandung"
+          />
+        </div>
+        <div class="col s12 m4">
+          <img
+            class="responsive-img popular-image"
+            src="../assets/images/derawan.jpeg"
+            alt="derawan"
+          />
+        </div>
+      </div>
+    </section>
     <section id="main-usp" class="main-usp">
-      <h4 class="blue-text text-lighten-1 center">Our Services</h4>
+      <h4 class="section-title blue-text text-lighten-1 center">Our Services</h4>
       <div class="row">
         <div class="col m6 s12">
           <i class="material-icons blue-text text-lighten-2 medium left">device_hub</i>
@@ -38,12 +73,26 @@
 </template>
 
 <style scoped>
-.row {
-  margin-top: 20px;
+.section-title {
+  margin-bottom: 30px;
 }
 
 .main-usp p {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.popular-location .row .col {
+    padding: 0.4rem;
+}
+
+.responsive-img {
+  height: 200px;
+  width: 100%;
+}
+
+.popular-image {
+  /* border: 1px solid; */
+  border-radius: 5px;
 }
 </style>
 
